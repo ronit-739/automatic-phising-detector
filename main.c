776f0509c39518248ea24ef5dd71c1a7ca05c8f1
Ronit Shrestha <stharonit88@gmail.com>
2025-03-10 21:54:18 +0545

    function added related to score checking

--- main.c
+++ main.c
@@ -1,12 +1,51 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
+    int count=0;
+    int total_function;
+    float percentage=30;
 
 
-printf("hello\n");
-printf("helooooo2");
+    // input 
+    //samrat part say gives input in variable a;
 
 
-    return 0;
+
+    //function call and record count if suspecious count++
+
+
+
+
+
+    // suspecious score calculation
+      percentage=(float)count/total_function*100;
+
+    // checking security perceentage  
+    if(percentage>0 &&  percentage <=30 )
+    {  printf("\nsafe!!\n");
+     
+    }
+
+    if(percentage>=31 && percentage<=60)
+    {  printf("\nSuspecious!!\n");
+       
+    }
+
+    if(percentage>=61 && percentage<=100)
+    {  printf("\nHigh Risk\n");
+       
+    } 
+    printf("---------------------------------------------");
+    printf("\ngiven url has  %d percantage chance of being a phising url\n ");
+
+
+
+
+    
+
+
+return 0;
+
+  
 }
